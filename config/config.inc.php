bd9e66aaa0d33d7faa5e35ec385e69130216da26
Miroku87 <miroku_87@yahoo.it>
2018-03-15 00:20:58 +0100

    Cambiato riferimento ai nomi delle grants

--- config/config.inc.php
+++ config/config.inc.php
@@ -3,9 +3,9 @@
 $DB_DATA = array(
 	"DB_DRIVER" => "mysql",
 	"DB_HOST"   => "localhost",
-	"DB_NAME"   => "reboot_live",
-	"DB_USER"   => "root",
-	"DB_PASS"   => "Ulb4br4b"
+	"DB_NAME"   => "reboot_live", //bl2o7rd1_reboot_live
+	"DB_USER"   => "root", //bl2o7rd1_dbuser
+	"DB_PASS"   => "Ulb4br4b" //R3b00t_us3r
 );
 
 ?>
